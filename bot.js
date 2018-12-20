c3ad6ce48e621711fd8f27a531938aa23d7f4c62
ahmedsaf <45786631+ahmedsaf@users.noreply.github.com>
2018-12-20 23:20:03 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("525413699176103978")
 setInterval(function() {
 channel.send(`AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED  AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED AHMED`);
 }, 30)
